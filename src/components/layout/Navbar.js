0bd5fa81b8745abfa15a036cb213c6f8ea111ef1
Anthony <chitowncoder@gmail.com>
2020-06-08 15:44:29 -0500

    Cards are rendering on screen, I have also used state, then refactored to use props

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,31 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types'
 
 
-export class Navbar extends Component {
-  static defaultProps ={
-    title: 'Github Finder',
-    icon: 'fa fa-github'
-  }
-
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired
-  }
-
-
-	render() {
+const Navbar = ({icon, title}) =>  {
 		return (
 			//*I HAVE USED BG-PRIMARY TO CHANGE THE COLOR OF THE NAV AREA
 			<nav className="navbar bg-primary">
 				<h1>
 					{/*  I HAVE USED FONTAWESOME */}
-					<i className={this.props.icon} />
-					{this.props.title}
+					<i className={icon} />
+					{title}
 				</h1>
 			</nav>
 		);
-	}
+
 }
+
+Navbar.defaultProps ={
+	title: 'Github Finder',
+	icon: 'fa fa-github-alt' /*CALLING FONT AWESOME HERE*/ 
+}
+
+Navbar.propTypes = {
+	title: PropTypes.string.isRequired,
+	icon: PropTypes.string.isRequired
+};
+
 
 export default Navbar;
