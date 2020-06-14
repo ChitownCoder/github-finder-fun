f2cb3320d35e4e1096b2d6731f2e93d66365cb2b
Anthony <chitowncoder@gmail.com>
2020-06-14 05:15:51 -0500

    I have added Router, pages are rendering no errors

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,30 +1,37 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
+const Navbar = ({ icon, title }) => {
+	return (
+		//*I HAVE USED BG-PRIMARY TO CHANGE THE COLOR OF THE NAV AREA
+		<nav className="navbar bg-primary">
+			<h1>
+				{/*  I HAVE USED FONTAWESOME */}
+				<i className={icon} />
+				{title}
+			</h1>
 
-const Navbar = ({icon, title}) =>  {
-		return (
-			//*I HAVE USED BG-PRIMARY TO CHANGE THE COLOR OF THE NAV AREA
-			<nav className="navbar bg-primary">
-				<h1>
-					{/*  I HAVE USED FONTAWESOME */}
-					<i className={icon} />
-					{title}
-				</h1>
-			</nav>
-		);
+			<ul>
+				<li>
+					<Link to="/">Home</Link>
+				</li>
+				<li>
+					<Link to="/about">About</Link>
+				</li>
+			</ul>
+		</nav>
+	);
+};
 
-}
-
-Navbar.defaultProps ={
+Navbar.defaultProps = {
 	title: 'Github Finder',
-	icon: 'fa fa-github-alt' /*CALLING FONT AWESOME HERE*/ 
-}
+	icon: 'fa fa-github-alt' /*CALLING FONT AWESOME HERE*/,
+};
 
 Navbar.propTypes = {
 	title: PropTypes.string.isRequired,
-	icon: PropTypes.string.isRequired
+	icon: PropTypes.string.isRequired,
 };
-
 
 export default Navbar;
